145d7ac73ba6e920320207debadb290ee1d03728
zhengxs <zhengxs2018@gmail.com>
2021-01-16 15:08:51 +0800

    添加端口号配置

--- apps/api/src/config/config.default.ts
+++ apps/api/src/config/config.default.ts
@@ -2,7 +2,7 @@ import type { ClientOpts } from 'redis'
 
 export default () => ({
   proxy: false,
-  port: 7000,
+  port: parseInt(process.env.PORT || '7100'),
   redis: <ClientOpts>{
     url: process.env.READS_URL,
   }
